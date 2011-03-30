57c13c440bdcec2e766daa19fa7a81759d60e1d2
Avi Kelman <fiendishx@gmail.com>
2011-03-30 08:58:58 +0000

    add xterm 256 color mode support

--- MUSHclient/worlds/plugins/aardwolf_colors.lua
+++ MUSHclient/worlds/plugins/aardwolf_colors.lua
@@ -9,9 +9,8 @@ local WHITE = 8
 local DEFAULT_COLOUR = "@w"
 
 -- colour styles (eg. @r is normal red, @R is bold red)
-
 local conversion = {
-  [GetNormalColour (BLACK)]   = "@k", -- note: aardwolf does not use this
+  [GetNormalColour (BLACK)]   = "@x000",
   [GetNormalColour (RED)]     = "@r",
   [GetNormalColour (GREEN)]   = "@g",
   [GetNormalColour (YELLOW)]  = "@y",
@@ -19,7 +18,7 @@ local conversion = {
   [GetNormalColour (MAGENTA)] = "@m",
   [GetNormalColour (CYAN)]    = "@c",
   [GetNormalColour (WHITE)]   = "@w",
-  [GetBoldColour   (BLACK)]   = "@D",
+  [GetBoldColour   (BLACK)]   = "@D", -- gray
   [GetBoldColour   (RED)]     = "@R",
   [GetBoldColour   (GREEN)]   = "@G",
   [GetBoldColour   (YELLOW)]  = "@Y",
@@ -27,7 +26,29 @@ local conversion = {
   [GetBoldColour   (MAGENTA)] = "@M",
   [GetBoldColour   (CYAN)]    = "@C",
   [GetBoldColour   (WHITE)]   = "@W",
-  
+    [526344] = "@x232",
+    [1184274] = "@x233",
+    [1842204] = "@x234",
+    [2500134] = "@x235",
+    [3158064] = "@x236",
+    [3815994] = "@x237",
+    [4473924] = "@x238",
+    [5131854] = "@x239",
+    [5789784] = "@x240",
+    [6447714] = "@x241",
+    [7105644] = "@x242",
+    [7763574] = "@x243",
+    [9079434] = "@x245",
+    [9737364] = "@x246",
+    [10395294] = "@x247",
+    [11053224] = "@x248",
+    [11711154] = "@x249",
+    [12369084] = "@x250",
+    [13027014] = "@x251",
+    [13684944] = "@x252",
+    [14342874] = "@x253",
+    [15000804] = "@x254",
+    [15658734] = "@x255",
   }  -- end conversion table
   
 -- This table uses the colours as defined in the MUSHclient ANSI tab, however the
@@ -49,9 +70,56 @@ colour_conversion = {
    B = GetBoldColour   (BLUE)    ,   -- 0xFF0000 
    M = GetBoldColour   (MAGENTA) ,   -- 0xFF00FF 
    C = GetBoldColour   (CYAN)    ,   -- 0xFFFF00 
-   W = GetBoldColour   (WHITE)   ,   -- 0xFFFFFF   
+   W = GetBoldColour   (WHITE)   ,   -- 0xFFFFFF
+    x232 = 526344,
+    x233 = 1184274,
+    x234 = 1842204,
+    x235 = 2500134,
+    x236 = 3158064,
+    x237 = 3815994,
+    x238 = 4473924,
+    x239 = 5131854,
+    x240 = 5789784,
+    x241 = 6447714,
+    x242 = 7105644,
+    x243 = 7763574,
+    x245 = 9079434,
+    x246 = 9737364,
+    x247 = 10395294,
+    x248 = 11053224,
+    x249 = 11711154,
+    x250 = 12369084,
+    x251 = 13027014,
+    x252 = 13684944,
+    x253 = 14342874,
+    x254 = 15000804,
+    x255 = 15658734,
 }  -- end conversion table
 
+-- Set up xterm color conversions
+xterm_intensities = {0x00, 0x33, 0x66, 0x99, 0xCC, 0xFF}
+for i = 1,8 do
+    colour_conversion[string.format("x%03d",i-1)] = GetNormalColour(i)
+    colour_conversion[string.format("x%02d",i-1)] = GetNormalColour(i)
+    colour_conversion[string.format("x%d",i-1)] = GetNormalColour(i)
+end
+for i = 9,16 do
+    colour_conversion[string.format("x%03d",i-1)] = GetBoldColour(i-8)
+    colour_conversion[string.format("x%02d",i-1)] = GetBoldColour(i-8)
+    colour_conversion[string.format("x%d",i-1)] = GetBoldColour(i-8)
+end
+for i = 17,231 do
+    BLUE = ((i-16)%6) + 1
+    GREEN = (math.floor((i-16)/6)%6) + 1
+    RED = (math.floor((i-16)/36)%6) + 1
+    xterm_colour = bit.shl(xterm_intensities[BLUE],16)+bit.shl(xterm_intensities[GREEN],8)+xterm_intensities[RED]
+    conversion[xterm_colour] = (conversion[xterm_colour] or string.format("@x%03d",i))
+    colour_conversion[string.format("x%03d",i)] = xterm_colour
+    colour_conversion[string.format("x%02d",i)] = xterm_colour
+    colour_conversion[string.format("x%d",i)] = xterm_colour
+end
+
+-- convert a line of style runs into color codes
 function StylesToColoursOneLine (styles, startcol, endcol)
   copystring = ""
   -- remove unneeded style runs at the start
@@ -62,7 +130,7 @@ function StylesToColoursOneLine (styles, startcol, endcol)
   end -- do
 
   -- nothing left? uh oh
-  if not next (styles) then return end
+  if not next (styles) then return copystring end
   
   -- discard unwanted part of first good style
   if startcol > 1 then
@@ -91,7 +159,8 @@ function StylesToColoursOneLine (styles, startcol, endcol)
     text = string.gsub (text, "~", "@-")
 
     -- put code in front, if we can find one
-    local code = conversion [styles [1].textcolour]
+    local code = conversion [styles[1].textcolour]
+
     if code then
       copystring = copystring .. code
     end -- if code found
@@ -105,32 +174,36 @@ function StylesToColoursOneLine (styles, startcol, endcol)
     table.remove (styles, 1)
   end -- while
   return copystring
-end -- DoOneLine
+end -- StylesToColoursOneLine
 
--- converts text with colour styles in it into style runs
-
+-- converts text with colour codes in it into style runs
 function ColoursToStyles (Text)
     if Text:match ("@") then
         astyles = {}
 
         Text = Text:gsub ("@%-", "~") -- fix tildes
         Text = Text:gsub ("@@", "\0") -- change @@ to 0x00
-        Text = Text:gsub ("@[^@cmyrgbwCMYRGBWD]", "")  -- rip out hidden garbage
-
+        Text = Text:gsub ("@x([^%d])","%1") -- strip invalid xterm codes
+        Text = Text:gsub ("@[^xcmyrgbwCMYRGBWD]", "")  -- rip out hidden garbage
+        
         -- make sure we start with @ or gsub doesn't work properly
         if Text:sub (1, 1) ~= "@" then
            Text = DEFAULT_COLOUR .. Text
         end -- if
 
         for colour, text in Text:gmatch ("@(%a)([^@]+)") do
-
            text = text:gsub ("%z", "@") -- put any @ characters back
 
+           if colour == "x" then -- xterm 256 colors
+              code,text = text:match("(%d%d?%d?)(.*)")
+              colour = colour..code
+           end
+           
            if #text > 0 then
               table.insert (astyles, { text = text, 
-                                       length = #text, 
-                                       textcolour = colour_conversion [colour] or GetNormalColour (WHITE),
-                                       backcolour = GetNormalColour (BLACK) })
+                  length = #text, 
+                  textcolour = colour_conversion [colour] or GetNormalColour (WHITE),
+                  backcolour = GetNormalColour (BLACK) })
            end -- if some text
         end -- for each colour run.
 
@@ -147,8 +220,9 @@ end  -- function ColoursToStyles
 function strip_colours (s)
   s = s:gsub ("@%-", "~")    -- fix tildes
   s = s:gsub ("@@", "\0")  -- change @@ to 0x00
-  s = s:gsub ("@[^@cmyrgbwCMYRGBWD]", "")  -- rip out hidden garbage
-  s = s:gsub ("@%a([^@]*)", "%1")
+  s = s:gsub ("@[^xcmyrgbwCMYRGBWD]", "")  -- rip out hidden garbage
+  s = s:gsub ("@x%d?%d?%d?", "") -- strip xterm color codes
+  s = s:gsub ("@%a([^@]*)", "%1") -- strip normal color codes
   return (s:gsub ("%z", "@")) -- put @ back
 end -- strip_colours
 
