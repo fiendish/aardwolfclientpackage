75f505479f9dfcd9a53b32081f29ddff80799c54
Avi Kelman <patcherton.fixesthings@gmail.com>
2014-04-20 01:33:45 -0400

    enable threading and fix ssl with new dlls
    Adds lua-llthreads so that we can get proper threading in plugins.
    This enables all kinds of greatness, like easy asynchronous networking,
    which we are now going to use in the update checker.
    Also fix the ssl dll with one I built myself. Apparently the old one
    didn't work on some machines (at least bare Windows XP).

---
+++ MUSHclient/lua/async.lua
@@ -0,0 +1,25 @@
+module("network", package.seeall)
+local _llthreads = require("llthreads")
+
+local network_thread_code = string.dump(function(arg)
+    local args = arg
+    local _socketeer = nil
+    if args.protocol == "HTTPS" then
+       _socketeer = require("ssl.https")
+    elseif args.protocol == "HTTP" then
+       _socketeer = require("socket.http")
+    else
+       return false
+    end
+
+    local page, status, headers, full_status = _socketeer.request(args.url)
+    return page, status, headers, full_status
+end)
+
+-- makes an asynchronous HTTP or HTTPS request to a URL
+function request(url, protocol)
+    thread = _llthreads.new(network_thread_code, {url=url, protocol=protocol})
+    thread:start()
+    return thread
+end
+
