b4c8e281adf4b97515f9530ca99f74d2c2f6a8b6
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2017-12-04 19:26:14 -0500

    pixels and colors

--- MUSHclient/lua/scrollbar.lua
+++ MUSHclient/lua/scrollbar.lua
@@ -76,12 +76,9 @@ function ScrollBar:draw(inside_callback)
    -- draw the background
    WindowCircleOp(
       self.window, miniwin.circle_rectangle,
-      self.left, self.top + self.width, self.left + self.width, self.top + self.height - self.width,
-      theme.INACTIVE_BODY, miniwin.pen_solid, 1,
-      theme.THREE_D_SURFACE, miniwin.brush_fine_pattern) -- brush
-
---   WindowRectOp(self.window, 2, self.left, self.top + self.width, self.left + self.width, self.top + self.height - self.width, theme.INACTIVE_BODY) -- scroll bar background
-   WindowRectOp(self.window, 1, self.left + 2, self.top + self.width + 2, self.left + self.width - 2, self.top + self.height - self.width - 2, theme.BODY_CONTRAST) -- scroll bar background inset rectangle
+      self.left, self.top + self.width + 1, self.left + self.width + 1, self.top + self.height - self.width,
+      theme.THREE_D_TRACK_COLOR1, miniwin.pen_solid, 1,
+      theme.THREE_D_TRACK_COLOR2, theme.THREE_D_TRACK_BRUSH) -- brush
 
    local mid_x = (self.width - 2)/2
 
@@ -91,15 +88,17 @@ function ScrollBar:draw(inside_callback)
    if (self.keepscrolling == "up") then
       Draw3DRect(self.window, self.left, self.top, self.left + self.width, self.top + self.width, true)
       points = string.format("%i,%i,%i,%i,%i,%i,%i,%i",
-         self.left + math.floor(mid_x) + 1, self.top + math.ceil(self.width/4 + 0.5) + 2, self.left + math.floor(mid_x) - math.floor(mid_x/2) + 1, self.top + round_banker(self.width/2) + 2,
-         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 1, self.top + round_banker(self.width/2) + 2,
-         self.left + math.ceil(mid_x) + 1, self.top + math.ceil(self.width/4 + 0.5) + 2 )
+         self.left + math.floor(mid_x) + 2, self.top + math.ceil(self.width/4 + 0.5) + 2,
+         self.left + math.floor(mid_x) - math.floor(mid_x/2) + 2, self.top + round_banker(self.width/2) + 2,
+         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 2, self.top + round_banker(self.width/2) + 2,
+         self.left + math.ceil(mid_x) + 2, self.top + math.ceil(self.width/4 + 0.5) + 2 )
    else
       Draw3DRect(self.window, self.left, self.top, self.left + self.width, self.top + self.width, false)
       points = string.format("%i,%i,%i,%i,%i,%i,%i,%i",
-         self.left + math.floor(mid_x), self.top + math.ceil(self.width/4 + 0.5) + 1, self.left + math.floor(mid_x) - math.floor(mid_x/2), self.top + round_banker(self.width/2) + 1,
-         self.left + math.ceil(mid_x) + math.floor(mid_x/2), self.top + round_banker(self.width/2) + 1,
-         self.left + math.ceil(mid_x), self.top + math.ceil(self.width/4 + 0.5) + 1)
+         self.left + math.floor(mid_x) + 1, self.top + math.ceil(self.width/4 + 0.5) + 1,
+         self.left + math.floor(mid_x) - math.floor(mid_x/2) + 1, self.top + round_banker(self.width/2) + 1,
+         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 1, self.top + round_banker(self.width/2) + 1,
+         self.left + math.ceil(mid_x) + 1, self.top + math.ceil(self.width/4 + 0.5) + 1)
    end
    WindowPolygon(self.window, points, theme.THREE_D_SURFACE_DETAIL, miniwin.pen_solid + miniwin.pen_join_miter, 1, theme.THREE_D_SURFACE_DETAIL, 0, true, false)
 
@@ -107,32 +106,34 @@ function ScrollBar:draw(inside_callback)
    if (self.keepscrolling == "down") then
       Draw3DRect(self.window, self.left, self.top + self.height - self.width, self.left + self.width, self.top + self.height, true)
       points = string.format("%i,%i,%i,%i,%i,%i,%i,%i",
-         self.left + math.floor(mid_x) + 1, self.top + self.height - 1 - math.ceil(self.width/4 + 0.5), self.left + math.floor(mid_x) - math.floor(mid_x/2) + 1, self.top + self.height - 1 - round_banker(self.width/2),
-         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 1, self.top + self.height - 1 - round_banker(self.width/2),
-         self.left + math.ceil(mid_x) + 1, self.top + self.height - 1 - math.ceil(self.width/4 + 0.5))
+         self.left + math.floor(mid_x) + 2, self.top + self.height - 1 - math.ceil(self.width/4 + 0.5),
+         self.left + math.floor(mid_x) - math.floor(mid_x/2) + 2, self.top + self.height - 1 - round_banker(self.width/2),
+         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 2, self.top + self.height - 1 - round_banker(self.width/2),
+         self.left + math.ceil(mid_x) + 2, self.top + self.height - 1 - math.ceil(self.width/4 + 0.5))
    else
       Draw3DRect(self.window, self.left, self.top + self.height - self.width, self.left + self.width, self.top + self.height, false)
       points = string.format("%i,%i,%i,%i,%i,%i,%i,%i",
-         self.left + math.floor(mid_x), self.top + self.height - 2 - math.ceil(self.width/4 + 0.5), self.left + math.floor(mid_x) - math.floor(mid_x/2), self.top + self.height - 2 - round_banker(self.width/2),
-         self.left + math.ceil(mid_x) + math.floor(mid_x/2), self.top + self.height - 2 - round_banker(self.width/2),
-         self.left + math.ceil(mid_x), self.top + self.height - 2 - math.ceil(self.width/4 + 0.5))
+         self.left + math.floor(mid_x) + 1, self.top + self.height - 2 - math.ceil(self.width/4 + 0.5),
+         self.left + math.floor(mid_x) - math.floor(mid_x/2) + 1, self.top + self.height - 2 - round_banker(self.width/2),
+         self.left + math.ceil(mid_x) + math.floor(mid_x/2) + 1, self.top + self.height - 2 - round_banker(self.width/2),
+         self.left + math.ceil(mid_x) + 1, self.top + self.height - 2 - math.ceil(self.width/4 + 0.5))
    end
-   WindowPolygon(self.window, points, 0x000000, 0, 1, 0x000000, 0, true, false)
+   WindowPolygon(self.window, points, theme.THREE_D_SURFACE_DETAIL, miniwin.pen_solid + miniwin.pen_join_miter, 1, theme.THREE_D_SURFACE_DETAIL, 0, true, false)
 
    -- draw the content indicator
    slots = math.max(0, self.total_steps - self.visible_steps)
    local position
-   local scroll_height = self.height - (2 * self.width)
+   local scroll_height = self.height - (2 * self.width) - 2
    if slots ~= 0 then
       self.size = math.max(10, scroll_height - slots)
       local available_space = scroll_height - self.size
       local space_per_step = available_space / slots
-      position = self.top + self.width + (space_per_step * (self.step-1))
-      if position + self.size > self.top + self.height - self.width then
-         position = self.height + self.top - self.size - self.width
+      position = self.top + self.width + math.ceil(space_per_step * self.step)
+      if position > self.top + self.height - self.width - self.size then
+         position = self.top + self.height - self.width - self.size
       end
    else
-      position = self.top + self.width
+      position = self.top + self.width + 1
       self.size = scroll_height
    end
    if (not self.has_hotspots) then
