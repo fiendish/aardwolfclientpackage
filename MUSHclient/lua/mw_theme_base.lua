2f8aa8edabf6611a300795a3df994b0dcdbca377
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2017-12-12 04:35:49 -0500

    more theme dev

--- MUSHclient/lua/mw_theme_base.lua
+++ MUSHclient/lua/mw_theme_base.lua
@@ -1,69 +1,73 @@
 --[[
-This file contains code that can be inherited by other plugins that allows
-many different miniwindow plugins to follow the same UI theme, as long as they
-follow certain guidelines. We can use this to maintain a base set of colors
+This file contains code that that allows many different miniwindow plugins
+to follow the same UI theme, as long as they follow certain guidelines.
+We can use this to maintain a base set of colors, standard display elements,
 and title fonts that get used everywhere in order to unify the visual style
 and provide a way to customize that style easily.
 
 Steps for use:
-1) From inside your other plugins, call: require "mw_theme_base".
-2) Use variable names as spelled out in mw_themes\default.lua
-3) Optional: Link some action in your plugin with either the choose_theme or the list_themes and load_theme functions.
-4) Optional: Make your own themes (copy lua/mw_themes/default.lua to a new *.lua file and customize the colors).
+1) Inside your plugins, require "mw_theme_base" at the very beginning of your script section.
+2) Use variable names as spelled out in mw_themes\default.lua for colorization
+3) Use the shared graphics functions defined below for drawing any 3D rectangles with the chosen color theme
+4) Call FinalizeThemes() at the very end of your script section.
+6) Optional: Make your own themes (copy lua/mw_themes/default.lua to a new *.lua file and customize the colors).
 --]]
+require "checkplugin"
 
 theme_dir = GetInfo(66).."lua\\mw_themes\\"
 theme_file = "default.lua"
 
-function list_themes ()
-   t, e = utils.readdir(theme_dir.."*.lua")
-   for k,v in pairs(t) do
-      t[k] = k:gsub("%.lua", ""):gsub("_", " ")
-   end
-   return t
+function b9315e040989d3f81f4328d6()
+   -- used for theme system detection
+   return true
 end
 
-function choose_theme ()
-   t = list_themes()
-   load_theme(
-      utils.listbox ("Which theme would you like to use?", "Choose Theme", t, theme_file)
-      or theme_file
-   )
+function get_theme()
+   return theme_file
 end
 
-function load_theme (file, no_broadcast)
-   status, theme = pcall(dofile, theme_dir..file)
+function load_theme (file)
+   status, theme = pcall(dofile, file)
 
    if status then
       theme_file = file
-      SetVariable("theme_file", theme_file)
-      if not no_broadcast then
-         BroadcastPlugin(1999, "new_theme:"..theme_file)
-      end
 
       theme.TITLE_FONT = "theme_title_font"
       theme.TITLE_FONTS = theme.TITLE_FONTS or {}
-      table.insert(theme.TITLE_FONTS, {["name"]="Dina", ["size"]=8}) -- in case other fonts aren't found
+      table.insert(theme.TITLE_FONTS, {["name"]="Dina", ["size"]=10}) -- in case other fonts aren't found
    end
 end
 
-function OnPluginBroadcast (msg, id, name, text)
-   if (msg == 1999) and (text:sub(1,10) == "new_theme:") then
-      load_theme(text:sub(11), true)
-   end
+local theme_controller = "b9315e040989d3f81f4328d6"
+if GetPluginID() ~= theme_controller and not IsPluginInstalled(theme_controller) then
+   local action = [[DoAfterSpecial(0.1, 'require \'checkplugin\';do_plugin_check_now(\']]..theme_controller..[[\', \'aard_Theme_Controller\')', sendto.script)]]
+   local prefix = GetAlphaOption("script_prefix")
+   action = [[
+      SetAlphaOption("script_prefix", "/")
+      Execute("/]]..action:gsub("\\", "\\\\")..[[")
+      SetAlphaOption("script_prefix", "]]..prefix:gsub("\\", "\\\\")..[[")
+   ]]
+   DoAfterSpecial(0.1, action, sendto.script)
 end
-
-load_theme(GetVariable("theme_file") or theme_file, true)
+theme_file = GetPluginVariable(theme_controller, "theme_file") or theme_file
+load_theme(theme_dir..theme_file)
 
 -- Replacement for WindowRectOp action 5, which allows for a 3D look while maintaining color theme.
-function Draw3DRect (win, left, top, right, bottom, sunken)
+function Draw3DRect (win, left, top, right, bottom, sunken, gradient)
    if right > 0 then
       right = right + 1
    end
    if bottom > 0 then
       bottom = bottom + 1
    end
-   WindowRectOp(win, 2, left, top, right, bottom, theme.THREE_D_SURFACE)
+   if gradient then
+      WindowGradient(win, left, top, right, bottom,
+                theme.TITLE_GRADIENT_FIRST,
+                theme.TITLE_GRADIENT_SECOND,
+                gradient)
+   else
+      WindowRectOp(win, 2, left, top, right, bottom, theme.THREE_D_SURFACE)
+   end
    if not sunken then
       WindowLine(win, left, top+1, right, top+1, theme.THREE_D_INNERHIGHLIGHT, 0x0200, 1)
       WindowLine(win, left+1, top, left+1, bottom, theme.THREE_D_INNERHIGHLIGHT, 0x0200, 1)
@@ -95,7 +99,7 @@ function DrawResizeTag (win, x1, y1, size)
     local m = 0
     local n = 0
     while (x1+m+2 <= x2-1 and y1+n+1 <= y2-2) do
-        WindowLine(win, x1+m+1, y2-2, x2-1, y1+n, theme.THREE_D_INNERHIGHLIGHT, 0, 1)
+        WindowLine(win, x1+m+1, y2-2, x2-1, y1+n, theme.THREE_D_OUTERHIGHLIGHT, 0, 1)
         WindowLine(win, x1+m+2, y2-2, x2-1, y1+n+1, theme.THREE_D_INNERSHADOW, 0, 1)
         m = m+3
         n = n+3
@@ -127,23 +131,24 @@ function DrawTitleBar(win, title, text_alignment, full_width)
       text_left = WindowInfo(win, 3) - title_width - theme.TITLE_PADDING
    end
    local text_right = math.min(text_left + title_width, WindowInfo(win, 3) - theme.TITLE_PADDING)
+
    if full_width then
-      Draw3DRect(
-         win,
-         0,
-         0,
-         WindowInfo(win, 3)-1,
-         theme.TITLE_HEIGHT-1
-      )
+      left = 0
+      right = WindowInfo(win, 3)-1
    else
-      Draw3DRect(
-         win,
-         math.max(0, text_left - theme.TITLE_PADDING),
-         0,
-         math.min(text_left + title_width + theme.TITLE_PADDING, WindowInfo(win, 3)),
-         theme.TITLE_HEIGHT
-      )
+      left = math.max(0, text_left - theme.TITLE_PADDING)
+      right = math.min(text_left + title_width + theme.TITLE_PADDING, WindowInfo(win, 3))
    end
+
+   Draw3DRect(
+      win,
+      left,
+      0,
+      right,
+      theme.TITLE_HEIGHT,
+      false,
+      theme.TITLE_GRADIENT
+   )
    WindowText(win, font, title, text_left, theme.TITLE_PADDING, text_right, theme.TITLE_HEIGHT, theme.THREE_D_SURFACE_DETAIL)
-   return theme.TITLE_HEIGHT
+   return theme.TITLE_HEIGHT+1
 end
