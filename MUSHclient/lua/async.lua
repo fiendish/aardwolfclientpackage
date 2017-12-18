8049c9aea9253a633e4826b567ad908024ae5247
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2017-12-18 01:52:56 -0500

    async request body?

--- MUSHclient/lua/async.lua
+++ MUSHclient/lua/async.lua
@@ -4,18 +4,18 @@ require "socket.http" -- just make sure they can load
 require "ssl.https" -- just make sure they can load
 
 local thread_pool = {}
-local request_urls = {}
+local requests = {}
 local request_times = {}
 local result_callbacks = {}
 local timeout_callbacks = {}
 local timeouts = {}
 
 -- This is really the function you should use instead of calling request directly.
--- result_callback_function gets arguments (retval, page, status, headers, full_status, requested_url)
+-- result_callback_function gets arguments (retval, page, status, headers, full_status, requested_url, request_body)
 -- callback_on_timeout function gets arguments (requested_url, timeout_after)
 -- request_protocol is either HTTP or HTTPS
 -- timeout_after is in seconds
-function doAsyncRemoteRequest(request_url, result_callback_function, request_protocol, timeout_after, callback_on_timeout)
+function doAsyncRemoteRequest(request_url, result_callback_function, request_protocol, timeout_after, callback_on_timeout, request_body)
    if request_protocol == nil then
       request_protocol = "HTTPS"
    end
@@ -31,9 +31,9 @@ function doAsyncRemoteRequest(request_url, result_callback_function, request_pro
    assert(type(result_callback_function) == "function" or type(result_callback_function) == "string")
    assert(type(callback_on_timeout) == "function" or type(callback_on_timeout) == "string" or callback_on_timeout == nil)
 
-   request_urls[thread_id] = request_url
+   requests[thread_id] = {['url']=request_url, ['body']=request_body}
    timeouts[thread_id] = timeout_after
-   thread_pool[thread_id] = request(request_url, request_protocol)
+   thread_pool[thread_id] = request(request_url, request_protocol, request_body)
    request_times[thread_id] = os.time()
 
    if type(result_callback_function) == "function" then
@@ -70,13 +70,13 @@ local network_thread_code = string.dump(function(arg)
        return false
     end
 
-    local page, status, headers, full_status = _socketeer.request(args.url)
+    local page, status, headers, full_status = _socketeer.request(args.url, args.body)
     return page, status, headers, full_status
 end)
 
 -- makes an asynchronous HTTP or HTTPS request to a URL
-function request(url, protocol)
-    local thread = _llthreads.new(network_thread_code, {url=url, protocol=protocol})
+function request(url, protocol, body)
+    local thread = _llthreads.new(network_thread_code, {url=url, protocol=protocol, body=body})
     thread:start()
     return thread
 end
@@ -86,7 +86,7 @@ function __checkCompletionFor(thread_id)
       if os.time() - request_times[thread_id] > timeouts[thread_id] then
          -- stop trying after timeout_after seconds
          local timeout_callback = timeout_callbacks[thread_id]
-         local request_url = request_urls[thread_id]
+         local request_data = requests[thread_id]
          local timeout = timeouts[thread_id]
 
          thread_pool[thread_id] = nil
@@ -94,19 +94,19 @@ function __checkCompletionFor(thread_id)
          result_callbacks[thread_id] = nil
          timeout_callbacks[thread_id] = nil
          timeouts[thread_id] = nil
-         request_urls[thread_id] = nil
+         requests[thread_id] = nil
 
          if timeout_callback ~= nil then
-            timeout_callback(request_url, timeout)
+            timeout_callback(request_data['url'], timeout, request_data['body'])
          else
-            default_timeout_callback(request_url, timeout)
+            default_timeout_callback(request_data['url'], timeout, request_data['body'])
          end
       else
          DoAfterSpecial(0.2, "async.__checkCompletionFor('"..thread_id.."')", sendto.script)
       end
    else
       local retval, page, status, headers, full_status = thread_pool[thread_id]:join()
-      local request_url = request_urls[thread_id]
+      local request_data = requests[thread_id]
       local callback_func = result_callbacks[thread_id]
 
       result_callbacks[thread_id] = nil
@@ -114,8 +114,8 @@ function __checkCompletionFor(thread_id)
       thread_pool[thread_id] = nil
       request_times[thread_id] = nil
       timeouts[thread_id] = nil
-      request_urls[thread_id] = nil
+      requests[thread_id] = nil
 
-      callback_func(retval, page, status, headers, full_status, request_url)
+      callback_func(retval, page, status, headers, full_status, request_data['url'], request_data['body'])
    end
 end
