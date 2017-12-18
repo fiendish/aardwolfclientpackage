b8e11c1f66e5ca530f4d56994d539b5408af2668
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2017-12-18 01:48:09 -0500

    theme development

--- MUSHclient/lua/mw_theme_base.lua
+++ MUSHclient/lua/mw_theme_base.lua
@@ -15,7 +15,7 @@ Steps for use:
 require "checkplugin"
 
 theme_dir = GetInfo(66).."lua\\mw_themes\\"
-theme_file = "default.lua"
+theme_file = "Charcoal.lua"
 
 function b9315e040989d3f81f4328d6()
    -- used for theme system detection
@@ -53,57 +53,81 @@ theme_file = GetPluginVariable(theme_controller, "theme_file") or theme_file
 load_theme(theme_dir..theme_file)
 
 -- Replacement for WindowRectOp action 5, which allows for a 3D look while maintaining color theme.
-function Draw3DRect (win, left, top, right, bottom, sunken, gradient)
+function Draw3DRect (win, left, top, right, bottom, depressed)
+   local gradient = (not theme.THREE_D_GRADIENT_ONLY_IN_TITLE or __theme_istitle) and theme.THREE_D_GRADIENT or false
+   __theme_istitle = false
+
    if right > 0 then
       right = right + 1
    end
    if bottom > 0 then
       bottom = bottom + 1
    end
-   if gradient then
+
+   if gradient and (theme.THREE_D_GRADIENT_FIRST == theme.THREE_D_GRADIENT_SECOND) then
+      gradient = false
+   end
+
+   if (gradient == 1) or (gradient == 2) or (gradient == 3) then
       WindowGradient(win, left, top, right, bottom,
-                theme.TITLE_GRADIENT_FIRST,
-                theme.TITLE_GRADIENT_SECOND,
+                theme.THREE_D_GRADIENT_FIRST,
+                theme.THREE_D_GRADIENT_SECOND,
                 gradient)
    else
-      WindowRectOp(win, 2, left, top, right, bottom, theme.THREE_D_SURFACE)
+      WindowRectOp(win, 2, left, top, right, bottom, theme.THREE_D_GRADIENT_FIRST)
    end
-   if not sunken then
-      WindowLine(win, left, top+1, right, top+1, theme.THREE_D_INNERHIGHLIGHT, 0x0200, 1)
-      WindowLine(win, left+1, top, left+1, bottom, theme.THREE_D_INNERHIGHLIGHT, 0x0200, 1)
 
-      WindowLine(win, left, bottom-2, right, bottom-2, theme.THREE_D_INNERSHADOW, 0x0200, 1)
-      WindowLine(win, right-2, top, right-2, bottom-2, theme.THREE_D_INNERSHADOW, 0x0200, 1)
+   if not depressed then
+      WindowLine(win, left+1, top+1, right, top+1, theme.THREE_D_HIGHLIGHT, 0x0200, 1)
+      WindowLine(win, left+1, top+1, left+1, bottom, theme.THREE_D_HIGHLIGHT, 0x0200, 1)
 
-      WindowLine(win, left, top, right, top, theme.THREE_D_OUTERHIGHLIGHT, 0x0200, 1)
-      WindowLine(win, left, top, left, bottom, theme.THREE_D_OUTERHIGHLIGHT, 0x0200, 1)
+      WindowLine(win, left, bottom-2, right, bottom-2, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
+      WindowLine(win, right-2, top, right-2, bottom-2, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
 
-      WindowLine(win, left, bottom-1, right, bottom-1, theme.THREE_D_OUTERSHADOW, 0x0200, 1)
-      WindowLine(win, right-1, top, right-1, bottom-1, theme.THREE_D_OUTERSHADOW, 0x0200, 1)
+      WindowLine(win, left, bottom-1, right, bottom-1, theme.THREE_D_HARDSHADOW, 0x0200, 1)
+      WindowLine(win, right-1, top, right-1, bottom-1, theme.THREE_D_HARDSHADOW, 0x0200, 1)
    else
-      WindowLine(win, left, top+1, right, top+1, theme.THREE_D_INNERSHADOW, 0x0200, 1)
-      WindowLine(win, left+1, top, left+1, bottom, theme.THREE_D_INNERSHADOW, 0x0200, 1)
+      WindowLine(win, left, top+1, right, top+1, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
+      WindowLine(win, left+1, top, left+1, bottom, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
 
-      WindowLine(win, left, top, right, top, theme.THREE_D_OUTERSHADOW, 0x0200, 1)
-      WindowLine(win, left, top, left, bottom, theme.THREE_D_OUTERSHADOW, 0x0200, 1)
+      WindowLine(win, left, top, right, top, theme.THREE_D_HARDSHADOW, 0x0200, 1)
+      WindowLine(win, left, top, left, bottom, theme.THREE_D_HARDSHADOW, 0x0200, 1)
 
-      WindowLine(win, left, bottom-1, right, bottom-1, theme.THREE_D_OUTERHIGHLIGHT, 0x0200, 1)
-      WindowLine(win, right-1, top, right-1, bottom-1, theme.THREE_D_OUTERHIGHLIGHT, 0x0200, 1)
+      WindowLine(win, left+1, bottom-1, right, bottom-1, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
+      WindowLine(win, right-1, top+1, right-1, bottom-1, theme.THREE_D_SOFTSHADOW, 0x0200, 1)
    end
 end
 
 -- the thing that goes in the bottom right corner for resizing miniwindows
-function DrawResizeTag (win, x1, y1, size)
-    local x2, y2 = x1+size, y1+size
-    Draw3DRect(win, x1, y1, x2, y2)
-    local m = 0
-    local n = 0
-    while (x1+m+2 <= x2-1 and y1+n+1 <= y2-2) do
-        WindowLine(win, x1+m+1, y2-2, x2-1, y1+n, theme.THREE_D_OUTERHIGHLIGHT, 0, 1)
-        WindowLine(win, x1+m+2, y2-2, x2-1, y1+n+1, theme.THREE_D_INNERSHADOW, 0, 1)
-        m = m+3
-        n = n+3
-    end
+function DrawResizeTag (win, type, x1, y1, size)
+   if not size then
+      size = theme.RESIZER_SIZE
+   end
+   local x2, y2
+   if not (x1 and y1) then
+      x2 = WindowInfo(win, 3) - 1
+      y2 = WindowInfo(win, 4) - 1
+      x1 = x2 - size - 1
+      y1 = y2 - size - 1
+   else
+      x2, y2 = x1+size, y1+size
+   end
+
+   if type == 2 then -- full
+      Draw3DRect(win, x1, y1, x2, y2, false)
+   else
+      WindowRectOp(win, miniwin.rect_fill, x1, y1, x2+1, y2+1, theme.BODY)
+   end
+
+
+   local m = 2
+   local n = 2
+   while (x1+m+1 <= x2 and y1+n+2 <= y2) do
+      WindowLine(win, x1+m, y2-1, x2, y1+n-1, theme.THREE_D_HIGHLIGHT, 0, 1)
+      WindowLine(win, x1+m+1, y2-1, x2, y1+n, theme.THREE_D_HARDSHADOW, 0, 1)
+      m = m+3
+      n = n+3
+   end
 end
 
 function TextHeight(win, font)
@@ -121,34 +145,46 @@ function LoadTitleFont(win)
    return theme.TITLE_FONT
 end
 
-function DrawTitleBar(win, title, text_alignment, full_width)
+function DressWindow(win, title, title_alignment, resizer_type)
+   if resizer_type then
+      DrawResizeTag(win, resizer_type)
+   end
+   local l, t, r, b = DrawBorder(win)
+   theme.TITLE_HEIGHT = 0
+   if title and Trim(title) ~= "" then
+      t = DrawTitleBar(win, title, title_alignment)
+   end
+   return l, t, r, b
+end
+
+function DrawTitleBar(win, title, text_alignment)
    local font = LoadTitleFont(win)
    local title_width = WindowTextWidth(win, font, title)
-   local text_left = (WindowInfo(win, 3) - title_width) / 2    -- text align center
+   local text_left = (WindowInfo(win, 3) - title_width) / 2    -- default text align center
    if text_alignment == "left" then
-      text_left = theme.TITLE_PADDING
+      text_left = theme.TITLE_PADDING + 10
    elseif text_alignment == "right" then
       text_left = WindowInfo(win, 3) - title_width - theme.TITLE_PADDING
    end
    local text_right = math.min(text_left + title_width, WindowInfo(win, 3) - theme.TITLE_PADDING)
 
-   if full_width then
-      left = 0
-      right = WindowInfo(win, 3)-1
-   else
-      left = math.max(0, text_left - theme.TITLE_PADDING)
-      right = math.min(text_left + title_width + theme.TITLE_PADDING, WindowInfo(win, 3))
-   end
-
+   __theme_istitle = true
    Draw3DRect(
       win,
-      left,
-      0,
-      right,
+      -1,
+      -1,
+      WindowInfo(win, 3),
       theme.TITLE_HEIGHT,
-      false,
-      theme.TITLE_GRADIENT
+      false
    )
    WindowText(win, font, title, text_left, theme.TITLE_PADDING, text_right, theme.TITLE_HEIGHT, theme.THREE_D_SURFACE_DETAIL)
    return theme.TITLE_HEIGHT+1
+end
+
+function DrawBorder(win)
+   local r = WindowInfo(win, 3)-3
+   local b = WindowInfo(win, 4)-3
+   WindowRectOp(win, 1, 0, 0, 0, 0, theme.THREE_D_HIGHLIGHT)
+   WindowRectOp(win, 1, 1, 1, -1, -1, theme.THREE_D_SOFTSHADOW)
+   return 2, 2, r, b
 end
