934c3ed3a8d95631f0612cf77f9181f092e534be
Avi Kelman <patcherton.fixesthings@gmail.com>
2018-07-29 20:39:01 -0400

    new theme deep red. make deep magenta more magenta to match.

--- MUSHclient/lua/mw_themes/Deep_Magenta.lua
+++ MUSHclient/lua/mw_themes/Deep_Magenta.lua
@@ -4,7 +4,7 @@
 return {
    LOGO_OPACITY = 0.03,
 
-   PRIMARY_BODY = 0x0c000c,
+   PRIMARY_BODY = 0x180018,
    SECONDARY_BODY = 0x552255,
    BODY_TEXT = 0xff00ff,
 
@@ -31,10 +31,34 @@ return {
    THREE_D_SURFACE_DETAIL = 0xff00ff, -- for contrasting details/text drawn on 3D surfaces
 
    -- for scrollbar background
-   SCROLL_TRACK_COLOR1 = 0x444444,
+   SCROLL_TRACK_COLOR1 = 0x331133,
    SCROLL_TRACK_COLOR2 = 0x664466,
    VERTICAL_TRACK_BRUSH = miniwin.brush_hatch_forwards_diagonal,
 
    DYNAMIC_BUTTON_PADDING = 20,
-   RESIZER_SIZE = 16
+   RESIZER_SIZE = 16,
+
+   -- bg_texture_function is optional to override the default behavior.
+   -- See Charcoal.lua for a "do nothing" variant.
+   -- Just make sure to return the path to a valid png file.
+   bg_texture_function = function()
+    imgpath = GetInfo(66).."worlds/plugins/images/bg1.png"
+
+    WindowCreate("WiLl_It_BlEnD", 0, 0, 0, 0, 0, 0, 0)
+    WindowLoadImage("WiLl_It_BlEnD", "tExTuRe", imgpath)
+    local tw = WindowImageInfo("WiLl_It_BlEnD", "tExTuRe", 2)
+    local th = WindowImageInfo("WiLl_It_BlEnD", "tExTuRe", 3)
+    WindowResize("WiLl_It_BlEnD", tw, th, Theme.THREE_D_HIGHLIGHT)
+    WindowImageFromWindow("WiLl_It_BlEnD", "cOlOr", "WiLl_It_BlEnD")
+
+    WindowDrawImage("WiLl_It_BlEnD", "tExTuRe", 0, 0, 0, 0, 1)
+    WindowFilter("WiLl_It_BlEnD", 0, 0, 0, 0, 7, 100)
+    WindowFilter("WiLl_It_BlEnD", 0, 0, 0, 0, 9, 4)
+    WindowBlendImage("WiLl_It_BlEnD", "cOlOr", 0, 0, 0, 0, 5, 0.8)
+
+    imgpath = GetInfo(66).."worlds/plugins/images/temp_theme_blend.png"
+    WindowWrite("WiLl_It_BlEnD", imgpath)
+
+    return imgpath
+ end
 }
